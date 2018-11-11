f6a603a500a380cbee88ceed9f8173e4365dbe1e
qiushui <chenminghuaht@163.com>
2018-11-11 23:46:05 +0800

    sync

---
+++ nettyServer/src/main/java/com/qiushui/study/netty/DiscardServerHandler.java
@@ -0,0 +1,23 @@
+package com.qiushui.study.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author: minghua
+ * @date: 2018/11/11 9:44
+ * @modified By:
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
